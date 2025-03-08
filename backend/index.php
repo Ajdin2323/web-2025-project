4902ecf62a7d8c0ff8753462422684991eed7575
Ajdin2323 <ajdin.karavdic@stu.ibu.edu.ba>
2025-03-08 20:49:54 +0100

    Implemented get all, get by id and delete users

--- backend/index.php
+++ backend/index.php
@@ -4,12 +4,14 @@ require 'vendor/autoload.php';
 
 //services
 require dirname(__FILE__).'/rest/services/ProductService.php';
+require dirname(__FILE__).'/rest/services/UserService.php';
 
 //routes
 require_once dirname(__FILE__).'/rest/routes/ProductRoutes.php';
+require_once dirname(__FILE__).'/rest/routes/UserRoutes.php';
 
 //register
 Flight::register('productService', 'ProductService');
-
+Flight::register('userService', 'UserService');
 
 Flight::start();
---
+++ backend/rest/routes/UserRoutes.php
@@ -0,0 +1,17 @@
+<?php
+
+Flight::route('GET /users(/@size(/@page))', function($size = null, $page = null) {
+    $size = $size ?? 10;
+    $page = $page ?? 1;
+
+    Flight::json(Flight::userService()->get_all_users($size, $page));
+});
+
+Flight::route('GET /user/@id', function($id) {
+    Flight::json(Flight::userService() -> get_user_by_id($id));
+});
+
+Flight::route('DELETE /user/@id', function($id) {
+    Flight::userService() -> delete_user($id);
+});
+
