de68be28203e0e765e62fec26ba236ae0a556b1e
Ajdin2323 <ajdin.karavdic@stu.ibu.edu.ba>
2025-03-14 13:31:07 +0100

    added category dropdown, added footer and fixed page size

--- frontend/assets/js/custom.js
+++ frontend/assets/js/custom.js
@@ -1,7 +1,5 @@
 $(document).ready(function() {
 
-  $("main#spapp > section").height($(document).height() - 60);
-
   var app = $.spapp({pageNotFound : 'error_404'}); // initialize
 
   // define routes
