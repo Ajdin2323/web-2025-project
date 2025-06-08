8512abe95b5f1f37b0c150299f6ee9e270ada17f
Ajdin2323 <ajdin.karavdic@stu.ibu.edu.ba>
2025-06-08 22:01:40 +0200

    Minor fix

--- backend/rest/services/UserService.php
+++ backend/rest/services/UserService.php
@@ -70,8 +70,6 @@ class UserService extends BaseService
             return;
         }
 
-        $users = $this->dao->find_user_by_email($email) ?? null;
-
         if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
             Flight::json(['message' => 'Email is not valid'], 400);
             return;
@@ -86,34 +84,38 @@ class UserService extends BaseService
 
         $hash = password_hash($password, PASSWORD_DEFAULT);
 
-        $data = $this->dao->add([
+        $this->dao->add([
             'first_name' => $first_name,
             'last_name' => $last_name,
             'email' => $email,
             'password' => $hash,
             'role' => Roles::USER,
         ]);
+
+        Flight::json(['message' => 'Registration successful'], 200);
     }
+
 
     public function login($data)
     {
         $email = $data['email'] ?? '';
         $password = $data['password'] ?? '';
 
-        if (!isset($email)) {
+        if (empty($email)) {
             Flight::json(['message' => 'Email is required'], 400);
             return;
         }
 
-        if (!isset($password)) {
+        if (empty($password)) {
             Flight::json(['message' => 'Password is required'], 400);
             return;
         }
 
-        $users = $this->dao->find_user_by_email($email) ?? null;
-        $user = $this->is_user_active($users);
+        $users = $this->dao->find_user_by_email($email);
 
-        if (!$user || !password_verify($password, $user['password'])) {
+        $active_user = $this->get_active_user($users);
+
+        if (!$active_user || !password_verify($password, $active_user['password'])) {
             Flight::json(['message' => 'Email or password is not correct'], 401);
             return;
         }
@@ -122,8 +124,8 @@ class UserService extends BaseService
         $expirationTime = $issuedAt + (7 * 24 * 60 * 60);
 
         $payload = [
-            'id' => $user['id'],
-            'role' => $user['role'],
+            'id' => $active_user['id'],
+            'role' => $active_user['role'],
             'iat' => $issuedAt,
             'exp' => $expirationTime
         ];
@@ -141,5 +143,15 @@ class UserService extends BaseService
             }
         }
         return false;
+    }
+
+    private function get_active_user($users)
+    {
+        foreach ($users as $user) {
+            if (!isset($user['deleted']) || $user['deleted'] == 0) {
+                return $user;
+            }
+        }
+        return null;
     }
 }
