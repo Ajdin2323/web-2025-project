487e9137fbd761c9495723c6a0c2392f6e6b0347
Ajdin2323 <ajdin.karavdic@stu.ibu.edu.ba>
2025-06-08 23:44:28 +0200

    Added product display for admin panel

--- frontend/assets/js/admin-panel.js
+++ frontend/assets/js/admin-panel.js
@@ -52,6 +52,59 @@ function loadUsers() {
   });
 }
 
+function loadProducts() {
+  const jwt = localStorage.getItem("token");
+  const user = decodeJWT(jwt);
+
+  if (!jwt || !user) {
+    alert("User not authenticated.");
+    return;
+  }
+
+  $.ajax({
+    url: `/web-2025-project/backend/products/100/1`,
+    method: "GET",
+    headers: {
+      Authentication: jwt,
+    },
+    success: function (response) {
+      const products = response.products;
+      const table = $("#productsTable");
+
+      if ($.fn.DataTable.isDataTable("#productsTable")) {
+        table.DataTable().clear().destroy();
+      }
+
+      const tableBody = table.find("tbody");
+      tableBody.empty();
+
+      products.forEach((product) => {
+        const row = `
+          <tr>
+            <td>
+              <img
+                src="${product.image}"
+                alt="${product.name}"
+                width="100"
+                height="100"
+                class="object-fit-cover"
+              />
+            </td>
+            <td>${product.name}</td>
+            <td>${product.price} BAM</td>
+          </tr>
+        `;
+        tableBody.append(row);
+      });
+
+      table.DataTable();
+    },
+    error: function () {
+      alert("Failed to load products.");
+    },
+  });
+}
+
 function capitalize(str) {
   return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
 }
@@ -61,7 +114,14 @@ $(document).ready(function () {
     loadUsers();
   });
 
+  $("#pills-home-tab").on("shown.bs.tab", function () {
+    loadProducts();
+  });
+
   if ($("#pills-profile").hasClass("active")) {
     loadUsers();
+  }
+  if ($("#pills-home").hasClass("active")) {
+    loadProducts();
   }
 });
